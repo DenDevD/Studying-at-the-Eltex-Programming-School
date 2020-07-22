a6c2adb18d3740b141edd8fcc08de8ed08ae3c3b
Denis Tyutkin <63635112+DenDevD@users.noreply.github.com>
2020-07-22 10:04:11 +0700

    Update main.c

--- Threads/main.c
+++ Threads/main.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
 #include <pthread.h>
 
+pthread_mutex_t mutex;
 long sum = 0;
 void *func(void *param)
 {
     long *ptr;
     ptr = (long *)param;
     long tmp;
+    pthread_mutex_lock(&mutex);
     for (long j = 0; j < 10000000; j++)
     {
         tmp = *ptr;
         tmp++;
         *ptr = tmp;
     }
+    pthread_mutex_unlock(&mutex);
     return 0;
 }
 
 int main()
 {
     pthread_t thread[500];
+    pthread_mutex_init(&mutex, NULL);
     for (int i = 0; i < 500; i++)
     {
         pthread_create(&thread[i], NULL, func, &sum);
@@ -27,6 +31,7 @@ int main()
     {
         pthread_join(thread[i], NULL);
     }
+    pthread_mutex_destroy(&mutex);
     printf("%ld\n", sum);
     return 0;
 }
